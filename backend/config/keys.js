39a8bde98e467bf4a5530a582cf4eb4773b80f97
Mandeep Pabla <mpabla7@gmail.com>
2020-10-09 13:23:20 -0700

    deleted files

--- backend/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://Stan:cs160@cs160-project.fddax.mongodb.net/CS160-ProjData?retryWrites=true&w=majority",
-};
--- frontend/src/actions/types.js
+++
@@ -1,3 +0,0 @@
-export const GET_USERS = "GET_USERS";
-export const ADD_USERS = "ADD_USER";
-export const USERS_LOADING = "USERS_LOADING";
