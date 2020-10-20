15529e57bc82d476353b2b5245ccba41a7e56508
victor <zell529@hotmail.com>
2020-10-20 16:07:25 -0700

    connected front end create study group to backend. Removed reactstrap

--- client/src/components/studygroup/CreateStudyGroup.js
+++ client/src/components/studygroup/CreateStudyGroup.js
@@ -1,159 +1,224 @@
 import React, { Fragment, useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import axios from "axios";
-import { Button, Form, FormGroup, Label, Input, FormText, Row, Col, Container} from 'reactstrap';
-import {connect} from 'react-redux';
+import { Button, Form, FormGroup, Label, Input, FormText, Row, Col, Container } from 'reactstrap';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import {login} from '../../actions/auth'; 
+import { createStudyGroup } from '../../actions/studygroup';
 import './styles.css';
 
-export const CreateStudyGroup = () => {
+export const CreateStudyGroup = ({ createStudyGroup }) => {
     const [formData, setFormData] = useState({
-        email: "",
-        password: "",
-      }); // hook
-    
-    const { email, password } = formData;
-    const onChange = (e) =>
-    setFormData({ ...formData, [e.target.name]: e.target.value }); // [e.target.name] makes it so we can use this for every feild dynamically
+        groupName: "",
+        startDate: "",
+        endDate: "",
+        repeating: "",
+        description: "",
+        courseId: "",
+        courseCode: "",
+    }); // hook
 
+    const { groupName, startDate, endDate, repeating, description, courseId, courseCode } = formData;
+    const onChange = (e) =>
+        setFormData({ ...formData, [e.target.name]: e.target.value }); // [e.target.name] makes it so we can use this for every feild dynamically
     const onSubmit = async (e) => {
         e.preventDefault();
-        //console.log("SUCCESS");
-        login(email, password);
-    };    
-
+        console.log("SUCCESS");
+        console.log()
+        createStudyGroup({ groupName, startDate, endDate, repeating, description, courseId, courseCode });
+    };
     return (
-      <Fragment>
-          <div className="create-group-form">
-            <h1>Create Study Group</h1>
-            <Form onSubmit={(e) => onSubmit(e)}>
-                <Form>
-                    <Row>
-                    <Col md="6">
-                        <FormGroup>
-                        <Label>Group Name </Label>
-                        <Input
-                            id="exampleFormControlInput1"
-                            placeholder="Enter Group Name"
-                            type="groupname"
-                            className="input-box"
-                            onChange={(e) => onChange(e)}
-                        ></Input>
-                        </FormGroup>
-                    </Col>
-                    </Row>
-                    <Row>
-                    <Col md="6">
-                        <FormGroup>
-                        <Label>Start Date </Label>
-                        <Input
-                            id="exampleFormControlInput2"
-                            placeholder="StartDate"
-                            type="startdate"
-                            className="input-box"
-                            onChange={(e) => onChange(e)}
-                        ></Input>
-                        </FormGroup>
-                    </Col>
-                    <Col md="6">
-                        <FormGroup>
-                        <Label>End Date </Label>
-                        <Input
-                            id="exampleFormControlInput3"
-                            placeholder="EndDate"
-                            type="enddate"
-                            className="input-box"
-                            onChange={(e) => onChange(e)}
-                        ></Input>
-                        </FormGroup>
-                    </Col>
-                    <Col md="6">
-                        <FormGroup>
-                        <Label>Repeating </Label>
-                        <Input
-                            id="exampleFormControlInput4"
-                            placeholder="Repeating"
-                            type="repeating"
-                            className="input-box"
-                            onChange={(e) => onChange(e)}
-                        ></Input>
-                        </FormGroup>
-                    </Col>
-                    </Row>
-                    <Col md="6">
-                        <FormGroup>
-                        <Label>Start Time </Label>
-                        <Input
-                            id="exampleFormControlInput5"
-                            placeholder="Start Time"
-                            type="starttime"
-                            className="input-box"
-                            onChange={(e) => onChange(e)}
-                        ></Input>
-                        </FormGroup>
-                    </Col>
-                    <Col md="6">
-                        <FormGroup>
-                        <Label>End Time </Label>
-                        <Input
-                            id="exampleFormControlInput6"
-                            placeholder="End Time"
-                            type="enddate"
-                            className="input-box"
-                            onChange={(e) => onChange(e)}
-                        ></Input>
-                        </FormGroup>
-                    </Col>
-                    <Col md="6">
-                        <FormGroup>
-                        <Label>Time Zone </Label>
-                        <Input
-                            id="exampleFormControlInput8"
-                            placeholder="Time Zone"
-                            type="timezone"
-                            className="input-box"
-                            onChange={(e) => onChange(e)}
-                        ></Input>
-                        </FormGroup>
-                    </Col>
-                    <Row>
-                    <Col md="6">
-                        <FormGroup>
-                        <Label>Meeting Link </Label>
-                        <Input
-                            id="exampleFormControlInput9"
-                            placeholder="Meeting Link"
-                            type="meetinglink"
-                            className="input-box"
-                            onChange={(e) => onChange(e)}
-                        ></Input>
-                        </FormGroup>
-                    </Col>
-                    </Row>
-                    <Row>
-                    <Col md="6">
-                        <FormGroup>
-                        <Label>Description </Label>
-                        <Input
-                            id="exampleFormControlInput10"
-                            placeholder="Enter Description"
-                            type="description"
-                            className="input-box"
-                            onChange={(e) => onChange(e)}
-                        ></Input>
-                        </FormGroup>
-                    </Col>
-                    </Row>
-                </Form>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
-            
-          </div>
-      </Fragment>
+        <Fragment>
+            {/* <div className="csg-create-group-form"> */}
+                <h1>Create Study Group!</h1>
+                <form onSubmit={(e) => onSubmit(e)}>
+                    <input
+                        type="text"
+                        className="csg-input-box"
+                        name="groupName"
+                        placeholder="Group Name"
+                        value={groupName}
+                        onChange={(e) => onChange(e)}
+                        required
+                    />
+
+                    <input
+                        type="text"
+                        className="csg-input-box"
+                        name="startDate"
+                        placeholder="Start Date"
+                        value={startDate}
+                        onChange={(e) => onChange(e)}
+                        required
+                    />
+                    <input
+                        type="text"
+                        className="csg-input-box"
+                        name="endDate"
+                        placeholder="End Date"
+                        value={endDate}
+                        onChange={(e) => onChange(e)}
+                    />
+                    <input
+                        type="text"
+                        className="csg-input-box"
+                        name="repeating"
+                        placeholder="Repeating"
+                        value={repeating}
+                        onChange={(e) => onChange(e)}
+                        required
+                    />
+                    <input
+                        type="text"
+                        className="csg-input-box"
+                        name="description"
+                        placeholder="Description"
+                        value={description}
+                        onChange={(e) => onChange(e)}
+                        minLength="6"
+                    />
+                    <input
+                        type="text"
+                        className="csg-input-box"
+                        name="courseId"
+                        placeholder="Course ID"
+                        value={courseId}
+                        onChange={(e) => onChange(e)}
+                        minLength="6"
+                    />
+                    <input
+                        type="text"
+                        className="csg-input-box"
+                        name="courseCode"
+                        placeholder="Course Code"
+                        value={courseCode}
+                        onChange={(e) => onChange(e)}
+                        minLength="6"
+                    />
+                    <p> </p>
+
+                    {/* <button type="submit" className="signup-btn" value="RegisterUser">
+                Sign up
+              </button> */}
+                    <input type="submit" className="btn btn-primary" value="Create Study Group!" />
+                </form>
+            {/* </div> */}
+        </Fragment>
     );
 };
-export default CreateStudyGroup;
-  
+export default connect(null, { createStudyGroup })(CreateStudyGroup);
+//     return (
+//       <Fragment>
+//           <div className="csg-create-group-form">
+//             <h1>Create Study Group</h1>
+//             <Form onSubmit={(e) => onSubmit(e)}>
+//                 <Form>
+//                     <Row>
+//                     <Col md="6">
+//                         <FormGroup>
+//                         <Label>Group Name </Label>
+//                         <Input
+//                             placeholder="Enter Group Name"
+//                             className="csg-input-box"
+//                             onChange={(e) => onChange(e)}
+//                         ></Input>
+//                         </FormGroup>
+//                     </Col>
+//                     </Row>
+//                     <Row>
+//                     <Col md="6">
+//                         <FormGroup>
+//                         <Label>Start Date </Label>
+//                         <Input
+//                             placeholder="StartDate"
+//                             className="csg-input-box"
+//                             onChange={(e) => onChange(e)}
+//                         ></Input>
+//                         </FormGroup>
+//                     </Col>
+//                     <Col md="6">
+//                         <FormGroup>
+//                         <Label>End Date </Label>
+//                         <Input
+//                             placeholder="EndDate"
+//                             className="csg-input-box"
+//                             onChange={(e) => onChange(e)}
+//                         ></Input>
+//                         </FormGroup>
+//                     </Col>
+//                     <Col md="6">
+//                         <FormGroup>
+//                         <Label>Repeating </Label>
+//                         <Input
+//                             placeholder="Repeating"
+//                             className="csg-input-box"
+//                             onChange={(e) => onChange(e)}
+//                         ></Input>
+//                         </FormGroup>
+//                     </Col>
+//                     </Row>
+//                     <Col md="6">
+//                         <FormGroup>
+//                         <Label>Start Time </Label>
+//                         <Input
+//                             placeholder="Start Time"
+//                             className="csg-input-box"
+//                             onChange={(e) => onChange(e)}
+//                         ></Input>
+//                         </FormGroup>
+//                     </Col>
+//                     <Col md="6">
+//                         <FormGroup>
+//                         <Label>End Time </Label>
+//                         <Input
+//                             placeholder="End Time"
+//                             className="csg-input-box"
+//                             onChange={(e) => onChange(e)}
+//                         ></Input>
+//                         </FormGroup>
+//                     </Col>
+//                     <Col md="6">
+//                         <FormGroup>
+//                         <Label>Time Zone </Label>
+//                         <Input
+//                             placeholder="Time Zone"
+//                             className="csg-input-box"
+//                             onChange={(e) => onChange(e)}
+//                         ></Input>
+//                         </FormGroup>
+//                     </Col>
+//                     <Row>
+//                     <Col md="6">
+//                         <FormGroup>
+//                         <Label>Meeting Link </Label>
+//                         <Input
+//                             placeholder="Meeting Link"
+//                             className="csg-input-box"
+//                             onChange={(e) => onChange(e)}
+//                         ></Input>
+//                         </FormGroup>
+//                     </Col>
+//                     </Row>
+//                     <Row>
+//                     <Col md="6">
+//                         <FormGroup>
+//                         <Label>Description </Label>
+//                         <Input
+//                             placeholder="Enter Description"
+//                             className="csg-input-box"
+//                             onChange={(e) => onChange(e)}
+//                         ></Input>
+//                         </FormGroup>
+//                     </Col>
+//                     </Row>
+//                 </Form>
+//                 <Button variant="primary" type="submit">
+//                     Submit
+//                 </Button>
+//             </Form>
+
+//           </div>
+//       </Fragment>
+//     );
+// };
+// export default connect(null, {createStudyGroup})(CreateStudyGroup);
